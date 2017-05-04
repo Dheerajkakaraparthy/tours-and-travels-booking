58baddb262da6a336f2d3b8bb8b0c39aaa8f5012
Dheerajkakaraparthy <dheerajkakaraparthy@gmail.com>
2017-05-05 00:23:44 +0530

    Update addadvertisement.php

--- addadvertisement.php
+++ addadvertisement.php
@@ -1,10 +1,5 @@
 <?php if(!isset($_SESSION)) { session_start(); } ?>
-<!--
-Author: W3layouts
-Author URL: http://w3layouts.com
-License: Creative Commons Attribution 3.0 Unported
-License URL: http://creativecommons.org/licenses/by/3.0/
--->
+
 <!DOCTYPE html>
 <html>
 <head>
